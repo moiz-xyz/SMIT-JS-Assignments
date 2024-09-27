ce8e2eb4be8ca6b663ea3ad60838ac55f5af8e37
Abdul Moiz <a.moizniaxi123@gmail.com>
2024-09-27 14:37:21 +0500

    Update main.js

--- Assignment 4/main.js
+++ Assignment 4/main.js
@@ -1,28 +1,28 @@
-// task no 1
-let a = 15;
-let b= 20;
-let c= a+b; 
-document.write("The sum of 15 & 20 is" +" "+ c)
+// // task no 1
+// let a = 15;
+// let b= 20;
+// let c= a+b; 
+// document.write("The sum of 15 & 20 is" +" "+ c)
 
-// task no 2 
+// // task no 2 
 
-// legal variables
-let name = "moiz";
-let fullName ="Abdul Moiz";
-let brith_year = 2006;
-let $sallry= "$2000";
-let saller2 ="15000";
+// // legal variables
+// let name = "moiz";
+// let fullName ="Abdul Moiz";
+// let brith_year = 2006;
+// let $sallry= "$2000";
+// let saller2 ="15000";
 
- /*Illegal variables
- let -Name = "Moiz";
- let full Name ="Abdul Moiz";
-let function :2006;
-let @sallry:"$200";
-let 2saller:"RS 1500"; */
+//  /*Illegal variables
+//  let -Name = "Moiz";
+//  let full Name ="Abdul Moiz";
+// let function :2006;
+// let @sallry:"$200";
+// let 2saller:"RS 1500"; */
 
-// task no 3 
-document.write("<h1> Rules for declaring variables </h1> <br>");
-document.write("Variables can only numbers,$ and _ For Example: $my_variable <br>");
-document.write("Varible must begain  with a letter, $ or _  For example $name ,_name , name <br>");
-document.write("Varibles name are case sensitive <br>");
-document.write("Variables name should not be JS keyword");
+// // task no 3 
+// document.write("<h1> Rules for declaring variables </h1> <br>");
+// document.write("Variables can only numbers,$ and _ For Example: $my_variable <br>");
+// document.write("Varible must begain  with a letter, $ or _  For example $name ,_name , name <br>");
+// document.write("Varibles name are case sensitive <br>");
+// document.write("Variables name should not be JS keyword");
