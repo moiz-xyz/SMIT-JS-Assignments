8c21741692d6b70838d15840741e2095785f3a35
Your Name <you@example.com>
2024-09-28 12:39:07 +0500

    updated index and js files

--- Assignment 12-13/main.js
+++ Assignment 12-13/main.js
@@ -58,28 +58,26 @@
 //     alert("You have entered an wrong password")
 // }
 
-// task no 6
- let name = prompt ("What is your name?")
-   let time = parseInt (prompt("What time is it in your country"));
-   var condition = prompt("Is it Am or Pm").toLowerCase();
-   if (time >=8 && time <= 11  && condition === "am"){
-      document.write( name +" "+"<br>" +"Good morning" +"<br>" + "May your day be filled with sunshine and success!")
-   } else if( time === 12 && condition === "pm" ){
-      document.write( name +" "+"<br>"+"<br>"+"Good afternoon"+"<br>"
-      + "Wishing you a wonderful afternoon filled with productivity and positivity!" )
-   }else if ( time >= 1 && time <= 5 && condition ==="pm"){
-      document.write( name +" "+"<br>"+"Good afternooon"+
-          "Wishing you a wonderful afternoon filled with productivity and positivity!")
-   }  else if ( time >= 5 && time <= 9 && condition === " pm"){   
-    document.write ( name +" " +"<br>" +"Good evening"+"<br>" +"Have a lovely evening, filled with relaxation and joy!") 
-} else if(time >=9 && time <= 12 &&  condition === "pm"){
-         document.write (  name +" "+"<br>"+"Good night"+"<br>"+ "May you have sweet dreams and restful sleep!")
-} else if( time === 12 && condition === "am" ){
-   document.write( name +" "+"<br>"+"Go to sleep"+"<br>"+"Good night! May you have sweet dreams and restful sleep!")
-}else if ( time >= 1 && time <= 7 && condition ==="am"){
-   document.write( name +" "+"<br>"+"Go to sleep"+"<br>"+"Good night! May you have sweet dreams and restful sleep!")
-}else {
-   document.write ( name +"  " +"<br>"+"Invalid time try to use 12 h format")
-}
-
-
+// // task no 6`
+//  let name = prompt ("What is your name?")
+//    let time = parseInt (prompt("What time is it in your country"));
+//    var condition = prompt("Is it Am or Pm").toLowerCase();
+//    if (time >=8 && time <= 11  && condition === "am"){
+//       document.write( name +" "+"<br>" +"Good morning" +"<br>" + "May your day be filled with sunshine and success!")
+//    } else if( time === 12 && condition === "pm" ){
+//       document.write( name +" "+"<br>"+"<br>"+"Good afternoon"+"<br>"
+//       + "Wishing you a wonderful afternoon filled with productivity and positivity!" )
+//    }else if ( time >= 1 && time <= 5 && condition ==="pm"){
+//       document.write( name +" "+"<br>"+"Good afternooon"+
+//           "Wishing you a wonderful afternoon filled with productivity and positivity!")
+//    }  else if ( time >= 5 && time <= 9 && condition === " pm"){   
+//     document.write ( name +" " +"<br>" +"Good evening"+"<br>" +"Have a lovely evening, filled with relaxation and joy!") 
+// } else if(time >=9 && time <= 12 &&  condition === "pm"){
+//          document.write (  name +" "+"<br>"+"Good night"+"<br>"+ "May you have sweet dreams and restful sleep!")
+// } else if( time === 12 && condition === "am" ){
+//    document.write( name +" "+"<br>"+"Go to sleep"+"<br>"+"Good night! May you have sweet dreams and restful sleep!")
+// }else if ( time >= 1 && time <= 7 && condition ==="am"){
+//    document.write( name +" "+"<br>"+"Go to sleep"+"<br>"+"Good night! May you have sweet dreams and restful sleep!")
+// }else {
+//    document.write ( name +"  " +"<br>"+"Invalid time try to use 12 h format")
+// }
---
+++ Assignmnet 14-16/main.js
@@ -0,0 +1,8 @@
+// let myName = "Abdul Moiz";
+// let myFname = " Ahmed nawaz";
+// let myMname = " Zeanat";
+
+// let family = [myName ,myFname ,  myMname ];
+// // alert(family);
+// let family1 = family.push("Niazi fanily");
+// alert(family);
